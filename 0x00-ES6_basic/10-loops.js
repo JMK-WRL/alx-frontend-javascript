0214a8815c3399b3045e8df9f04dd9b716fb7330
Jonathan Kyule <jonathankyule2@gmail.com>
2024-04-08 19:46:29 +0000

    task 10 updates

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
-  }
+export default function appendToEachArrayValue(array, appendstring) {
+	const a = [];
+	for (const value of array) {
+		a.push(appenstring + value);
+	}
 
-  return array;
+	return a;
 }
